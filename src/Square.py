b8038be6f47d0ba2c18da71899e094fb371b9ce3
solovevaanastasia <anasatsiya.solo563@gmail.com>
2024-01-18 20:12:00 +0300

    ООП на практике 2

--- src/Square.py
+++ src/Square.py
@@ -6,4 +6,7 @@ class Square(Rectangle):
         if side_a <= 0:
             ValueError("Сторона квадрата должна быть больше 0 ")
         super().__init__(side_a, side_a)
+        self.name = "Square"
         self.side_a = side_a
+
+
